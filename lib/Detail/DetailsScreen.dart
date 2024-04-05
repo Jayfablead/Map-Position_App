257fb03c496e34a49ff959795ea13e68319232f9
Jay Fablead <jay.fablead@gmail.com>
2024-04-05 18:24:45 +0530

    04/05/2024

--- lib/Detail/DetailsScreen.dart
+++ lib/Detail/DetailsScreen.dart
@@ -9,7 +9,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:mapposition/Extras/Const.dart';
 import 'package:readmore/readmore.dart';
-import 'package:sizer/sizer.dart';
+import 'package:sizer/sizer.dart';`
 
 import '../Extras/Drwer.dart';
 import '../Extras/Loader.dart';
